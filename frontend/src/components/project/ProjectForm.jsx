d36112ffaaa06334612811ec67c8850dba35a013
Erison <103046444+erisonk1@users.noreply.github.com>
2025-02-08 07:28:08 -0300

    Reapply "Começo da produção do frontend"
    This reverts commit 77e4e08c12738ce34d3b62cb5aa4e210e58a6b1f.

---
+++ frontend/src/components/project/ProjectForm.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+import style from "./ProjectForm.module.css";
+import Input from '../form/Input'
+import SubmitButton from '../form/SubmitButton'
+function ProjectForm({ btnText }) {
+  
+
+  return (
+    <form className={style.form}>
+      <Input
+        type="text"
+        text="Nome do projeto"
+        name="name"
+        placeholder="Insira o nome do Projeto"
+
+      />
+      <Input className={style.description}
+        type="text"
+        text="Descrição do projeto"
+        name="description"
+        placeholder="Insira a Descrição"
+      />
+
+      <SubmitButton text={btnText} />
+    </form>
+  );
+}
+export default ProjectForm;
